ca3531473bbde2e697bf13e35a7729284dd42a8b
chandankumar <chandankumar@localhost>
2024-12-31 17:16:12 +0530

    Revert "Added runner file"
    This reverts commit 5ea3c97f5691fd0eeef181ff568c772d69a54da3.

--- src/test/java/stepDefinitions/registrationSteps.java
+++ src/test/java/stepDefinitions/registrationSteps.java
@@ -7,12 +7,11 @@ public class registrationSteps {
 	@Given("I launch the application")
 	public void launch()
 	{
-		System.out.println("application launched");
 		
 	}
 	@When ("I click the registeration link")
 	public void click()
 	{
-		System.out.println("registration link clicked");
+		
 	}
 }
