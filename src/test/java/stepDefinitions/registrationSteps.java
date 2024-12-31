5ea3c97f5691fd0eeef181ff568c772d69a54da3
chandankumar <chandankumar@localhost>
2024-12-31 17:10:05 +0530

    Added runner file

--- src/test/java/stepDefinitions/registrationSteps.java
+++ src/test/java/stepDefinitions/registrationSteps.java
@@ -7,11 +7,12 @@ public class registrationSteps {
 	@Given("I launch the application")
 	public void launch()
 	{
+		System.out.println("application launched");
 		
 	}
 	@When ("I click the registeration link")
 	public void click()
 	{
-		
+		System.out.println("registration link clicked");
 	}
 }
